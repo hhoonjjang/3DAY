90f47ea3f67e8001d57926fd71f45d39f7b95b4b
hhoonjjang <111038171+hhoonjjang@users.noreply.github.com>
2022-11-13 20:02:18 +0900

    1113

--- 3DAYMarket/routes/chat.js
+++ 3DAYMarket/routes/chat.js
@@ -1,8 +1,12 @@
+const Cryptojs = require("crypto-js");
+const jwt = require("jsonwebtoken");
 const router = require("express").Router();
 
+const { Chat, Item } = require("../models/index.js");
 router.post("/", async (req, res) => {
   try {
     console.log(req.body);
+    res.send(req.body);
   } catch (err) {
     console.log(err);
   }
@@ -13,7 +17,7 @@ router.post("/sendinfo", async (req, res) => {
     console.log(req.body.tempTure);
     console.log(req.body.me);
     console.log(req.body.partner);
-
+    console.log(req.body);
     res.send({
       name: req.body.me,
       patner: req.body.partner,
