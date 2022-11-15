f50afbea2d52abc2492c01253f5e83689a4b528d
YoungjunKim <color99b@naver.com>
2022-11-15 09:47:02 +0900

    add search all page

--- 3DAYMarket/public/chatting/index.js
+++ 3DAYMarket/public/chatting/index.js
@@ -255,8 +255,27 @@ const reverse = function () {
 
 reverse();
 
-document.getElementById("search").onkeydown = (event) => {
-  if (window.event.keyCode == 13) {
-    location.href = "http://localhost:8080/search";
+// document.getElementById("search").onkeydown = (event) => {
+//   if (window.event.keyCode == 13) {
+//     location.href = "http://localhost:8080/search";
+//   }
+// };
+
+const searchAddress = "http://localhost:8080/search/?result=";
+let filterItemList = [];
+const searchItem = document.forms["search-form"];
+searchItem.onsubmit = async function (event) {
+  event.preventDefault();
+
+  if (filterItemList) {
+    for (let i = 0; i < filterItemList.length; i++) {
+      filterItemList.pop();
+    }
   }
+  console.log(filterItemList);
+  const filterItemListSend = await axios.post("/api/item/filterItem", {
+    list: filterItemList,
+  });
+
+  location.href = searchAddress + searchItem.search.value;
 };
